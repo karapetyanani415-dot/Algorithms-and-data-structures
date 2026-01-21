5dc653883e9afce1a26b2d5cbc26b1e70ec4e771
karapetyanani415-dot <karapetyanani415@gmail.com>
2026-01-21 18:22:38 +0400

    Bubble Sort Algorithms

---
+++ Algorithms/Bubble Sort/bubble_sort.js
@@ -0,0 +1,21 @@
+function bubble_sort(arr) {
+    let cmp = true
+    let n = arr.length
+    for (let i = 0; i < arr.length - 1; ++i) {
+        cmp = false
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                cmp = true
+            }
+        }
+        if (!cmp) {
+            break
+        }
+    }
+}
+
+let arr = [5, 4, 3, 2, 1]
+bubble_sort(arr)
+console.log(arr)
+

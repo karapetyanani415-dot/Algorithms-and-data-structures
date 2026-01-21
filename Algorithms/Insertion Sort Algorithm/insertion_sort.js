269b3fefe3038551358ee62c05373a6b273e8d38
karapetyanani415-dot <karapetyanani415@gmail.com>
2026-01-21 18:24:43 +0400

    Insertion Sort Algorithm

---
+++ Algorithms/Insertion Sort Algorithm/insertion_sort.js
@@ -0,0 +1,15 @@
+function insertion_sort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+let arr = [12, 11, 13, 5, 6];
+insertion_sort(arr);
+console.log(arr)

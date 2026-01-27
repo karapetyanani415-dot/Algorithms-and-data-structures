386fed84a6434e5377aea58cd08b1988f6401387
karapetyanani415-dot <karapetyanani415@gmail.com>
2026-01-27 19:43:53 +0400

    Binary Search

--- Algorithms/Binary Search/binary_search.c
+++ Algorithms/Binary Search/binary_search.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
-int binary_search(int arr[], int target, int n)
-{
+int binary_search(int arr[], int target, int n) {
     int start = 0;
     int end = n - 1;
     while (start <= end) {
-        int mid = 
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] > target) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
     }
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int idx = binary_search(arr, 7, n);
+    printf("%d\n", idx); 
+
+    return 0;
 }
